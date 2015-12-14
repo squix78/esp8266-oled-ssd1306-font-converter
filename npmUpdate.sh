8efd1b4375bf23126465282847cba6342aaa1fae
Daniel Eichhorn <dani.eichhorn@squix.ch>
2015-12-14 21:16:27 +0100

    adapted path new hooks

--- npmUpdate.sh
+++ npmUpdate.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-PATH=/var/lib/openshift/566d81410c1e6676960001c5/app-root/data/node-v4.2.3-linux-x64/bin/:$PATH
+PATH=/var/lib/openshift/566d81410c1e6676960001c5/app-root/data/node-v5.2.0-linux-x64/bin/:$PATH
 export HOME=$HOME/app-root/runtime/repo
 npm -v
-npm cache clean
 npm install bower
 npm install --only=dev
 bower install
