3bcd1482113e4db00bd6527bfa6f709903016164
Daniel Eichhorn <dani.eichhorn@squix.ch>
2015-12-17 20:45:40 +0100

    commented out npm stuff

--- npmUpdate.sh
+++ npmUpdate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 PATH=/var/lib/openshift/566d81410c1e6676960001c5/app-root/data/node-v5.2.0-linux-x64/bin/:$PATH
 export HOME=$HOME/app-root/runtime/repo
-npm -v
-npm install bower
-source "\$OPENSHIFT_REPO_DIR/.openshift/lib/setup_custom_nodejs_env"
-npm install --only=dev
-bower install
-grunt
+#npm -v
+#npm install bower
+#source "\$OPENSHIFT_REPO_DIR/.openshift/lib/setup_custom_nodejs_env"
+#npm install --only=dev
+#bower install
+#grunt
