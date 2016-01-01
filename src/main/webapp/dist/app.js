8c1febcb794d7504be3965487612d3712a220ed2
Daniel Eichhorn <dani.eichhorn@squix.ch>
2016-01-01 20:48:15 +0100

    Added spinner

--- src/main/webapp/dist/app.js
+++ src/main/webapp/dist/app.js
@@ -44,9 +44,11 @@ angular.module('FontConverter').controller('MainController',
 	  $scope.fontArray.name = "Dialog";
 	  $scope.fontArray.style = "0";
 	  $scope.fontArray.size = 16;
-	  
+	  $scope.isDataReady = false;
 	  $scope.fontFamilies = FontFamilies.query();
-	  $scope.fonts = Fonts.query({fontFamily: $scope.fontArray.fontFamily});
+	  $scope.fonts = Fonts.query({fontFamily: $scope.fontArray.fontFamily}, function() {
+		  $scope.isDataReady = true;
+	  });
 	  console.log("Arrived in font converter: " + $scope.fontArray);
 	  $scope.getFontArray = function() {
 		 $scope.fontArray.$save(function() {
